f89d58dd37b974dce172c447ff75d5df52272621
Robyn Shae Fong <robyn.fong@outlook.com>
2025-05-21 12:37:49 +0930

    Implementing a horizontal pixel sort
    Currently the edge mask isnt working but its getting there.

--- src/imgfkr/__init__.py
+++ src/imgfkr/__init__.py
@@ -5,7 +5,12 @@ import numpy as np
 
 def main() -> None:
     greyscale = cv2.imread("./data/in.jpg", cv2.IMREAD_GRAYSCALE)
+    colour = cv2.imread("./data/in.jpg", cv2.IMREAD_COLOR)
     
     result = core.contrastMask(greyscale, 40, 180)
 
     cv2.imwrite("./data/out/out.jpg", result)
+
+    result = core.hSort(colour, result)
+
+    cv2.imwrite("./data/out/out2.jpg", result)
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,52 @@
-use ndarray::{Dim, Zip};
+use ndarray::{s, Dim, Zip};
 use numpy::ndarray::{self, Array, ArrayView};
-use numpy::{Ix2, PyArray, PyArrayMethods, ToPyArray};
+use numpy::{Ix2, Ix3, PyArray, PyArrayMethods, ToPyArray};
 use pyo3::prelude::*;
 use pyo3::{pymodule, types::PyModule, Bound, PyResult, Python};
 use std::ops::Range;
+
+fn h_sort<'a>(colour: ArrayView<'a, u8, Ix3>, mask: ArrayView<'a, u8, Ix2>) -> Array<usize, Ix2> {
+    let mut _result: Array<u8, Ix3> = Array::zeros(colour.raw_dim());
+
+    // TODO: Put this into its own method
+    let mut tallies: Array<usize, Ix2> = Array::zeros(mask.raw_dim());
+
+    let mut i = 0;
+    for mut row in tallies.rows_mut() {
+        let mask_row = mask.row(i);
+        i += 1;
+        for mut i in 0..row.iter().count() {
+            if mask_row[i] == u8::MAX {
+                let idx = (&mask_row.slice(s![i..]))
+                    .iter()
+                    .position(|a| *a == 0)
+                    .unwrap_or((&mask_row.slice(s![i..])).iter().count() + 1)
+                    - 1;
+                row[i] = idx;
+                i += idx;
+
+                println!("idx = {}", idx);
+            }
+        }
+    }
+    // /TODO
+
+
+    return tallies;
+}
+
+#[pyfunction]
+#[pyo3(name = "hSort")]
+fn h_sort_py<'py>(
+    py: Python<'py>,
+    colour: &Bound<'py, PyArray<u8, Ix3>>,
+    mask: &Bound<'py, PyArray<u8, Ix2>>,
+) -> PyResult<Bound<'py, PyArray<usize, Dim<[usize; 2]>>>> {
+    let colour = unsafe { colour.as_array() };
+    let mask = unsafe { mask.as_array() };
+    Ok(h_sort(colour, mask).to_pyarray(py))
+}
+
 
 fn contrast_mask<'a>(greyscale: ArrayView<'a, u8, Ix2>, range: Range<u8>) -> Array<u8, Ix2> {
     let mut result: Array<u8, Ix2> = Array::zeros(greyscale.raw_dim());
@@ -39,5 +82,6 @@ fn contrast_mask_py<'py>(
 #[pymodule]
 fn core(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(contrast_mask_py, m)?)?;
+    m.add_function(wrap_pyfunction!(h_sort_py, m)?)?;
     Ok(())
 }
