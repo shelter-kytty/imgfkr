d1415a66294e5d6e25c6b1ccabfc54d4fbf4b3e4
Robyn Shae Fong <robyn.fong@outlook.com>
2025-05-24 18:53:11 +0930

    Forgot hsl comparators
    Implemented conversion functions for rgb to hsl, used for sorting pixels. Now actually on par with the original script.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,20 @@ use std::iter::zip;
 use std::ops::Range;
 use std::vec::Vec;
 
+trait SortedImpl {
+    fn sorted(self) -> Self;
+}
+
+impl<T> SortedImpl for Vec<T>
+where
+    T: std::cmp::PartialOrd,
+{
+    fn sorted(mut self) -> Self {
+        self.sort_unstable_by(|a, b| T::partial_cmp(a, b).expect("Vec<T>::sorted() failed"));
+        self
+    }
+}
+
 fn contrast_gradient<'a>(mask: ArrayView<'a, u8, Ix2>) -> Array<usize, Ix2> {
     let mut tallies: Array<usize, Ix2> = Array::zeros(mask.raw_dim());
 
@@ -35,6 +49,61 @@ fn contrast_gradient_py<'py>(
 ) -> PyResult<Bound<'py, PyArray<usize, Dim<[usize; 2]>>>> {
     let mask = unsafe { mask.as_array() };
     Ok(contrast_gradient(mask).to_pyarray(py))
+}
+
+fn calc_luminance(rgb: Vec<u8>) -> f64 {
+    assert!(rgb.len() == 3);
+    let rgb = rgb
+        .iter()
+        .map(|i| (*i as f64) / 255.0)
+        .collect::<Vec<f64>>()
+        .sorted();
+    return (rgb[0] + rgb[2]) / 2.0;
+}
+
+fn calc_saturation(rgb: Vec<u8>) -> f64 {
+    assert!(rgb.len() == 3);
+    let rgb = rgb
+        .iter()
+        .map(|i| (*i as f64) / 255.0)
+        .collect::<Vec<f64>>()
+        .sorted();
+
+    if rgb.first() == rgb.last() {
+        return 0.0;
+    } else {
+        let lum = (rgb[0] + rgb[2]) / 2.0;
+
+        if lum > 0.5 {
+            return (rgb[0] - rgb[2]) / (rgb[0] + rgb[2]);
+        } else {
+            return (rgb[0] - rgb[2]) / (2.0 - rgb[0] - rgb[2]);
+        }
+    }
+}
+
+fn calc_hue(rgb: Vec<u8>) -> f64 {
+    assert!(rgb.len() == 3);
+
+    fn inner(rgb: Vec<u8>) -> f64 {
+        let rgb = rgb
+            .iter()
+            .map(|i| (*i as f64) / 255.0)
+            .collect::<Vec<f64>>();
+        let sorted = rgb.clone().sorted();
+
+        if rgb[0] == sorted[0] {
+            return (rgb[1] - rgb[2]) / (sorted[0] - sorted[2]);
+        } else if rgb[1] == sorted[0] {
+            return 2.0 + (rgb[2] - rgb[0]) / (sorted[0] - sorted[2]);
+        } else {
+            return 4.0 + (rgb[0] - rgb[1]) / (sorted[0] - sorted[2]);
+        }
+    }
+
+    let r = inner(rgb);
+
+    return if r > 0.0 { r * 60.0 } else { r * 60.0 + 360.0 };
 }
 
 fn h_edge_mask<'a>(mask: ArrayView<'a, u8, Ix2>) -> Array<usize, Ix2> {
@@ -88,12 +157,16 @@ fn h_sort<'a>(colour: ArrayView<'a, u8, Ix3>, mask: ArrayView<'a, u8, Ix2>) -> A
                         sorted.push(vec![vector[i], vector[i + 1], vector[i + 2]]);
                     }
                 }
-                // sorting by "most red" atm
-                sorted.sort_by(|a, b| (a.first().unwrap()).cmp(&b.first().unwrap()));
+
+                sorted.sort_by(|a, b| {
+                    let a = calc_saturation(a.clone());
+                    let b = calc_saturation(b.clone());
+                    f64::partial_cmp(&a, &b).expect("Total order not found (colour sort)")
+                });
                 let sorted = Array::from_shape_vec(view.raw_dim(), sorted.concat()).unwrap();
 
                 let mut r: ArrayViewMut2<u8> = rrow.slice_mut(s![i..i + erow[i], ..]);
-                
+
                 for (a, b) in zip(r.iter_mut(), sorted.iter()) {
                     *a = *b;
                 }
@@ -116,11 +189,15 @@ fn h_sort_py<'py>(
     Ok(h_sort(colour, mask).to_pyarray(py))
 }
 
-fn contrast_mask<'a>(greyscale: ArrayView<'a, u8, Ix2>, range: Range<u8>) -> Array<u8, Ix2> {
+fn contrast_mask<'a>(greyscale: ArrayView<'a, u8, Ix2>, range: Range<u32>) -> Array<u8, Ix2> {
     let mut result: Array<u8, Ix2> = Array::zeros(greyscale.raw_dim());
 
     Zip::from(greyscale).and(&mut result).for_each(|a, b| {
-        *b = if range.contains(a) { u8::MAX } else { 0 };
+        *b = if range.contains(&(*a as u32)) {
+            u8::MAX
+        } else {
+            0
+        };
     });
 
     return result;
@@ -131,15 +208,15 @@ fn contrast_mask<'a>(greyscale: ArrayView<'a, u8, Ix2>, range: Range<u8>) -> Arr
 fn contrast_mask_py<'py>(
     py: Python<'py>,
     greyscale: &Bound<'py, PyArray<u8, Ix2>>,
-    min: u8,
-    max: u8,
+    min: u32,
+    max: u32,
 ) -> PyResult<Bound<'py, PyArray<u8, Dim<[usize; 2]>>>> {
     let greyscale = unsafe { greyscale.as_array() };
     Ok(contrast_mask(
         greyscale,
         Range {
             start: min,
-            end: max + 1,
+            end: max,
         },
     )
     .to_pyarray(py))
